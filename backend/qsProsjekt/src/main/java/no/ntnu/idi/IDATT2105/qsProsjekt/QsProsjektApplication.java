401f7f5ad30a5c14386beb437eed1f62f4badd0d
Ida Angell Veglo Klæstad <iaklaest@stud.ntnu.no>
2022-03-25 15:56:19 +0100

    intial commit

---
+++ backend/qsProsjekt/src/main/java/no/ntnu/idi/IDATT2105/qsProsjekt/QsProsjektApplication.java
@@ -0,0 +1,16 @@
+package no.ntnu.idi.IDATT2105.qsProsjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+@SpringBootApplication
+public class QsProsjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsProsjektApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}

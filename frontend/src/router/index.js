f935d762d4e63513f9f823eafde429a94a94d22c
Ida Angell Veglo Klæstad <iaklaest@stud.ntnu.no>
2022-03-26 11:40:36 +0100

    started frontend work

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import LoginPage from "../views/LoginPage.vue"
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/",
+    name: "LoginPage",
+    component: LoginPage,
   },
   {
     path: "/about",
---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="loginContainer" @submit.prevent="handleClickSignin">
+    <BaseInput
+      id="inpUsername"
+      class="input"
+      v-model="event.username"
+      label="Username"
+      type="text"
+    />
+
+    <BaseInput
+      id="inpPassword"
+      class="input"
+      v-model="event.password"
+      label="Password"
+      type="password"
+    />
+
+    <BaseButton id="btnSubmit" class="button" @click="handleClickSignin">
+      Login
+    </BaseButton>
+  </div>
+</template>
+<script>
+import BaseInput from "@/components/BaseInput";
+import BaseButton from "@/components/BaseButton.vue"
+import { doLogin } from "../utils/apiutil";
+export default {
+  name: "LoginInfo",
+  components: {
+    BaseInput,
+    BaseButton,
+  },
+  data() {
+    return {
+      event: {
+        username: "",
+        password: "",
+        message: "Looks like you need to be registered!",
+      },
+      loginFailed: false,
+      loginStatus: "",
+    };
+  },
+  methods: {
+    async handleClickSignin() {
+      //alert("You entered, username: " + this.event.username);
+      const loginRequest = {
+        username: this.event.username,
+        password: this.event.password,
+      };
+      const LoginResponse = await doLogin(loginRequest);
+      if (LoginResponse.loginStatus == "Success") {
+        this.$store.commit("set_login_username", loginRequest.username);
+        this.$router.push("/homepage");
+      } else this.loginFailed = true;
+    },
+  },
+};
+</script>
+<style scoped>
+#loginContainer {
+  display: grid;
+  justify-content: center;
+  margin: 40px;
+}
+
+#loginTitle {
+  font-size: x-large;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+#username,
+#password {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 20px;
+}
+</style>
